8e85fc3286de8f93c0abb1c8048cd4ed66fff49b
mpomery <bob_george33@hotmail.com>
2014-12-03 17:43:40 +0800

    camera.php works now
    Needs to have right owner on the server

--- www/camera.php
+++ www/camera.php
@@ -1,11 +1,12 @@
 <?php
-	$camera = ($_GET['camera']?$_GET['camera']:'nocamera');
+	$camera = 'nocamera';
+	if (isset($_GET['camera']))
+			$camera=$_GET['camera'];
 	$imagelocation="../";
 	$file = $imagelocation.strtolower($camera).".jpg";
 	header("Expires: ".gmdate("D, d M Y H:i:s", time())." GMT");
-	if (!is_readable($file)) {
+	if (!is_readable($file))
 		$file="nocamera.jpg";
-	}
 	header("Content-Type: image/jpeg");
 	header("Content-Length: ".filesize($file));
 	header("Last-Modified: ".gmdate("D, d M Y H:i:s", filectime($file))." GMT");
